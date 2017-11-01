1bf435ccd061646287bfa2bbe908219f9f21b79c
zhangyanwei <zhangyanwei@zbj.com>
2017-11-01 19:21:44 +0800

    复习

--- lessons/01-setting-up/index.js
+++ lessons/01-setting-up/index.js
@@ -1,4 +1,18 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { Router, Route, hashHistory } from 'react-router'
 import App from './modules/App'
-render(<App/>, document.getElementById('app'))
+import About from './modules/About'
+import Repos from './modules/Repos'
+import Param from './modules/Param'
+
+// render(<App/>, document.getElementById('app'))
+render( 
+    <Router history={hashHistory}>
+        <Route path="/" component={App}>
+            <Route path="/repos" component={Repos}/>
+            <Route path="/about" component={About}/>
+            <Route path="/any/:params" component={Param}/>
+        </Route>
+    </Router> 
+,document.getElementById('app'))
--- lessons/01-setting-up/modules/App.js
+++ lessons/01-setting-up/modules/App.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import NavLink from './NavLink'
 
 export default React.createClass({
   render() {
-    return <div>Hello, React Router!</div>
+    return <div>
+      <ul role="nav">
+        <li><NavLink to="/about">About</NavLink></li>
+        <li><NavLink to="/repos">Repos</NavLink></li>
+        <li><NavLink to="/any">Param</NavLink></li>
+      </ul>
+      {this.props.children}
+    </div>
   }
 })
